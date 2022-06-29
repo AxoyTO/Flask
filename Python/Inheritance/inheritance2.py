eb6019bcdf2689881e9f2e15a82d891f5dc8e424
Tevfik Oguzhan Aksoy <toaxoy@protonmail.com>
2022-06-29 15:48:33 +0300

    Formatted

--- Python/Inheritance/inheritance2.py
+++ Python/Inheritance/inheritance2.py
@@ -3,11 +3,11 @@ from typing import Set
 
 
 class Complex(object):
-    'this class simulates complex numbers'
+    "this class simulates complex numbers"
 
     def __init__(self, real, imaginary):
-        if(type(real) not in (int, float)) or type(imaginary) not in (int, float):
-            raise Exception('Args are not int or float!')
+        if (type(real) not in (int, float)) or type(imaginary) not in (int, float):
+            raise Exception("Args are not int or float!")
         self.SetReal(real)
         self.SetImaginary(imaginary)
 
@@ -18,18 +18,18 @@ class Complex(object):
         return self.__imaginary
 
     def Modulus(self):
-        return math.sqrt(self.GetReal()**2 + self.GetImaginary()**2)
+        return math.sqrt(self.GetReal() ** 2 + self.GetImaginary() ** 2)
 
     def Phi(self):
         return math.atan2(self.GetImaginary(), self.GetReal())
 
     def SetReal(self, val):
-        if(type(val) not in (int, float)):
+        if type(val) not in (int, float):
             raise Exception("Val has to be int or float!")
         self.__real = val
 
     def SetImaginary(self, val):
-        if(type(val) not in (int, float)):
+        if type(val) not in (int, float):
             raise Exception("Val has to be int or float!")
         self.__imaginary = val
 
--- Python/Visualization/panda.py
+++ Python/Visualization/panda.py
@@ -1,24 +1,20 @@
 import pandas as pd
 
-df = pd.read_csv('AirPassengers.csv')
-print(len(df['time']))
+df = pd.read_csv("AirPassengers.csv")
+print(len(df["time"]))
 # print(df['time'])
 # print(df['value'])
 
-names = ['Wade', 'James', 'Kobe', 'Curry', 'Ben', 'Messi', 'Karaman']
+names = ["Wade", "James", "Kobe", "Curry", "Ben", "Messi", "Karaman"]
 total = [55, 50, 44, 36, 100, 91, 32]
 data_set = list(zip(names, total))
 print(names)
 print(total)
 print(data_set)
-data_frame = pd.DataFrame(data=data_set, columns=['Names', 'Total'])
-data_frame.to_csv('points', index=True, header=True)
-data_frame.to_json('points.json', index=True)
-df = pd.DataFrame(zip(names, total), columns=['Players', 'Total'])
+data_frame = pd.DataFrame(data=data_set, columns=["Names", "Total"])
+data_frame.to_csv("points", index=True, header=True)
+data_frame.to_json("points.json", index=True)
+df = pd.DataFrame(zip(names, total), columns=["Players", "Total"])
 with pd.ExcelWriter("points.xlsx") as writer:
     df.to_excel(writer)
 print(data_frame.memory_usage())
-
-papi = pd.read_excel('C:\\Users\\toaxo\\Desktop\\Sem5\\HPC\\Parallel_Programming\\Task2_PAPI\\PAPI_Data.xlsx',
-                     keep_default_na=False, na_filter=False, sheet_name=0)
-print(papi)
