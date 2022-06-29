f124f67c55e9a5d3cee8a5b222fe9ba2bc39a1b4
Tevfik Oguzhan Aksoy <toaxoy@protonmail.com>
2022-06-29 15:43:56 +0300

    SocketProgramming TODO

--- Python/SocketProgramming/server.py
+++ Python/SocketProgramming/server.py
@@ -6,7 +6,7 @@ HEADER = 64
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
-FORMAT = 'utf-8'
+FORMAT = "utf-8"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
