d9ebfbd9b43d631e6caece1c21538bc7a8d89db7
AxoyTO <toaxoy@protonmail.com>
2022-07-09 09:41:08 +0300

    Bcrypt import for user auth

--- Projects/flask-blog/flaskblog/__init__.py
+++ Projects/flask-blog/flaskblog/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 from flaskblog.config import SECRET_KEY
 
@@ -9,5 +10,6 @@ app.config['SECRET_KEY'] = SECRET_KEY
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///site.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
