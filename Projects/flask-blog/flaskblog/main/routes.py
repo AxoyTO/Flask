8a77b98e75839c7a5844b638b8da09f2bf917d74
AxoyTO <toaxoy@protonmail.com>
2022-07-18 15:28:25 +0300

    Added sorting posts functionality

--- Projects/flask-blog/flaskblog/main/routes.py
+++ Projects/flask-blog/flaskblog/main/routes.py
@@ -1,16 +1,33 @@
 from flaskblog.models import Post
 from flask import render_template, request, Blueprint
+from flaskblog.posts.forms import SortPostsForm
 
 main_bp = Blueprint('main', __name__)
 
 
-@main_bp.route("/")
-@main_bp.route("/home")
+@main_bp.route("/", methods=['GET', 'POST'])
+@main_bp.route("/home", methods=['GET', 'POST'])
 def home():
     page = request.args.get("page", 1, type=int)
     posts = Post.query.order_by(
         Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template("home.html", posts=posts)
+    form = SortPostsForm()
+    if form.sort_by.data == "date_posted_new":
+        posts = Post.query.order_by(
+            Post.date_posted.desc()).paginate(page=page, per_page=5)
+    elif form.sort_by.data == "date_posted_old":
+        posts = Post.query.order_by(
+            Post.date_posted.asc()).paginate(page=page, per_page=5)
+    elif form.sort_by.data == "title":
+        posts = Post.query.order_by(
+            Post.title.asc()).paginate(page=page, per_page=5)
+    elif form.sort_by.data == "user":
+        posts = Post.query.order_by(
+            Post.user_id.asc()).paginate(page=page, per_page=5)
+    elif form.sort_by.data == "priority":
+        posts = Post.query.order_by(
+            Post.is_announcement.desc()).paginate(page=page, per_page=5)
+    return render_template("home.html", posts=posts, form=form)
 
 
 @main_bp.route("/about", endpoint="about")
--- Projects/flask-blog/flaskblog/posts/forms.py
+++ Projects/flask-blog/flaskblog/posts/forms.py
@@ -1,10 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, TextAreaField, SubmitField, BooleanField, SelectField
 from wtforms.validators import DataRequired
 
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     content = TextAreaField('Content', validators=[DataRequired()])
+    announcement = BooleanField('Announcement')
 
     submit = SubmitField('Post')
+
+
+class SortPostsForm(FlaskForm):
+    options = [('date_posted_new', 'Date posted (Newest first)'),
+               ('date_posted_old', 'Date posted (Oldest first)'),
+               ('title', 'Title'),
+               ('user', 'User'),
+               ('priority', 'Priority')]
+    sort_by = SelectField('Sort posts by:', choices=options)
+    submit = SubmitField('Sort')
