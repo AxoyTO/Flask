0d7b7e9be6f3a21dab1ce650758397a175d7621e
Tevfik Oguzhan Aksoy <toaxoy@protonmail.com>
2022-07-27 14:42:07 +0300

    DB credentials config added

--- Projects/question_answer/qna/config.py
+++ Projects/question_answer/qna/config.py
@@ -1,8 +1,22 @@
 import os
+from configparser import ConfigParser
+
+
+def config(filename='../database.ini', section='postgresql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(
+            'Section {0} not found in the {1} file'.format(section, filename))
+
+    return db
 
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLAlchemy_DATABASE_URI = os.environ.get('DATABASE_URL')
-    EMAIL_USER = os.environ.get('EMAIL_USER')
-    EMAIL_PASS = os.environ.get('EMAIL_PASS')
