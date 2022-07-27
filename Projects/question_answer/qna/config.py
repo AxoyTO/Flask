015dd9b7ccc3b51045cadada914e34dca413997a
Tevfik Oguzhan Aksoy <toaxoy@protonmail.com>
2022-07-27 09:28:28 +0300

    Added new env vars

--- Projects/question_answer/qna/config.py
+++ Projects/question_answer/qna/config.py
@@ -4,3 +4,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLAlchemy_DATABASE_URI = os.environ.get('DATABASE_URL')
+    EMAIL_USER = os.environ.get('EMAIL_USER')
+    EMAIL_PASS = os.environ.get('EMAIL_PASS')
