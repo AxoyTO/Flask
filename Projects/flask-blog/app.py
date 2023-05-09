beda88d1aab72003ba3e682cd82bdf7d86f045d4
Oguz Aksoy <oguzhan.aksoy@1ci.com>
2023-05-09 20:26:43 +0300

    docker upd

--- Projects/flask-blog/app.py
+++ Projects/flask-blog/app.py
@@ -1,7 +1,11 @@
 from venv import create
 from flaskblog import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True)  # debug=True for development
+    if not "SECRET_KEY" in os.environ:
+        from secrets import token_hex
+        os.environ["SECRET_KEY"] = token_hex(16)
+    app.run(debug=True, host="0.0.0.0")  # debug=True for development
